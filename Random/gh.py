dfacbaa0c3d636bcc17f05845583a6858989d450
bmaca <novmaca@gmail.com>
2015-07-26 22:16:14 -0500

    Added a new .py script

---
+++ Random/gh.py
@@ -0,0 +1,27 @@
+###Open a Github repo from the command line by passing the repo name as an argument. 
+
+import json
+import requests
+import sys
+import webbrowser
+
+
+
+name = sys.argv[1]
+
+
+url = "https://api.github.com/search/repositories?q=%s&fork=false&in=html_url&order=desc" % name
+
+try:
+    data = requests.get( url)
+except requests.exceptions.RequestException as e:
+    print e
+    sys.exit(1)
+
+x = data.json()
+
+print x["items"][0]["html_url"]
+
+repo = x["items"][0]["html_url"]
+
+webbrowser.open(repo)
